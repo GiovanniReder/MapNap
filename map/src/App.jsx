091010c2a88a6d05b02a798d298d68c1138e91c4
Giovanni <gio.reder@gmail.com>
2024-07-16 17:15:33 +0200

    fix response of CardCamping

--- map/src/App.jsx
+++ map/src/App.jsx
@@ -4,9 +4,18 @@ import CardCamping from "./components/CardCamping";
 export default function App() {
   return (
     <>
-      <CardCamping />
-      <Map />
-      <p>testo</p>
+      <div className="container">
+        <div className="row">
+          <div className=" d-none d-lg-block col-md-2 col-xl-3">
+            <CardCamping />
+          </div>
+          <div className=" col-md-2 col-xl-1"></div>
+          <div className="p-0 col-12 col-md-8 col-xl-8">
+            <Map />
+          </div>
+          <p>testo</p>
+        </div>
+      </div>
     </>
   );
 }
--- map/src/components/CardCamping.jsx
+++ map/src/components/CardCamping.jsx
@@ -1,36 +1,26 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
-const imageStyle = {
-  maxWidth: "100%",
-  height: "auto",
-  borderRadius: "8px",
-};
-const logoStyle = {
-  maxWidth: "10%",
-  height: "auto",
-  borderRadius: "8px",
-};
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/CardCamping.css";
 
 const CardCamping = () => {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card className="border  border-2 border-success" style={{ width: "19rem" }}>
       <Card.Img
         variant="top"
         src="https://www.habitante.it/wp-content/uploads/2019/07/shutterstock_649280029-1-e1562258471502.jpg"
-        style={imageStyle}
+        className="imageStyle"
       />
       <Card.Body>
-        <Card.Img
-          variant="top"
-          src="https://images.vexels.com/media/users/3/128996/isolated/preview/05b6f24fdd179e6be9df30522d2843f9-vintage-camping-rounded-seal.png"
-          style={logoStyle}
-        />
-        <Card.Title>La Campeggia</Card.Title>
+        <div style={{ display: "flex" }}>
+          <Card.Img
+            className="logoStyle bg-white rounded rounded-pill border border-4 border-white"
+            variant="top"
+            src="https://images.vexels.com/media/users/3/128996/isolated/preview/05b6f24fdd179e6be9df30522d2843f9-vintage-camping-rounded-seal.png"
+          />
+          <Card.Title className="ms-1">La Campeggia</Card.Title>
+        </div>
         <Card.Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur vitae iusto maiores
-          accusantium placeat quis nemo mollitia, quidem excepturi dignissimos molestias hic assumenda quas ratione
-          numquam corporis earum cum?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur vitae iusto maiores accus
         </Card.Text>
       </Card.Body>
     </Card>
